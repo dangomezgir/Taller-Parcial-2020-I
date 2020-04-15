6e3d0a62545089ed1ba415703cd642431a43e545
Pablo Bejarano <pabhoz@gmail.com>
2020-04-15 18:35:00 -0500

    Base

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+define('INTERFACES',__DIR__.'/interfaces/');
+define('FACTORIES',__DIR__.'/factories/');
+define('MODELS',__DIR__.'/models/');
+
+require_once './loader.php';
+
+
