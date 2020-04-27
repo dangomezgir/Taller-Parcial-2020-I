a2cf09610be72e5f989e7f2f4d0a806f4ddb89f3
SpiceLag <spicelag@gmail.com>
2020-04-26 21:15:32 -0500

    Ultimo commit

--- index.php
+++ index.php
@@ -8,16 +8,25 @@
 require_once './config.php';
 require_once './mvcBootstrap.php';
 
-//$char = CharacterFactory::newRogue("Percy");
-//$char->create();
+// $char1 = CharacterFactory::newMage("charmander");
+// $char2->create();
+//  $char2 = CharacterFactory::newWarrior("Arlakesh");
+//  $char2->create();
+// $char3 = CharacterFactory::newRogue("Percy");
+// $char3->create();
+
 $charmander = CharacterFactory::getCharacter(1);
-$arlakesh = CharacterFactory::getCharacter(2);
-$percy = CharacterFactory::getCharacter(4);
-//$percy->oneLevelUp();
-//$percy->update();
-// print_r($charmander);
-// print_r($arlakesh);
-// print_r($percy);
+$arlakesh = CharacterFactory::getCharacter(4);
+$percy = CharacterFactory::getCharacter(5);
+
+$percy->oneLevelUp();
+$percy->update();
+print_r($charmander);
+print_r($arlakesh);
+print_r($percy);
+// $percy->attack($arlakesh);
+// $percy->attack($arlakesh);
+// $percy->attack($arlakesh);
 // $percy->attack($arlakesh);
 
 
